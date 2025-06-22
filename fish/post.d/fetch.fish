7154d487f1bbd82d0da86c786a58795173f4dd7c
Uyanide <pywang0608@foxmail.com>
2025-06-22 08:56:52 +0200

    fetch: increace width

--- fish/post.d/fetch.fish
+++ fish/post.d/fetch.fish
@@ -4,16 +4,16 @@ end
 
 if test "$fetch_logo_type" = "symbols"
     set -g fetch_args "--logo-type raw --logo-width 42 --logo \"$HOME/.config/fastfetch/logo_ros/42x.symbols\" --color \"#74c7ec\""
-    set -g fetch_args_brief "--logo-type raw --logo-width 24 --logo \"$HOME/.config/fastfetch/logo_ros/24x.symbols\" --color \"#74c7ec\""
+    set -g fetch_args_brief "--logo-type raw --logo-width 28 --logo \"$HOME/.config/fastfetch/logo_ros/28x.symbols\" --color \"#74c7ec\""
 else if test "$fetch_logo_type" = "logo"
     set -g fetch_args "--logo-type builtin"
     set -g fetch_args_brief "--logo-type small"
 else if test "$fetch_logo_type" = "sixel"
     set -g fetch_args "--logo-type raw --logo-width 42 --logo \"$HOME/.config/fastfetch/logo_ros/42x.sixel\" --color \"#74c7ec\""
-    set -g fetch_args_brief "--logo-type raw --logo-width 24 --logo \"$HOME/.config/fastfetch/logo_ros/24x.sixel\" --color \"#74c7ec\""
+    set -g fetch_args_brief "--logo-type raw --logo-width 28 --logo \"$HOME/.config/fastfetch/logo_ros/28x.sixel\" --color \"#74c7ec\""
 else # "kitty" or "auto" and others
     set -g fetch_args "--logo-type $fetch_logo_type --logo-width 42 --logo \"$HOME/.config/fastfetch/logo_ros/ros.png\" --color \"#74c7ec\""
-    set -g fetch_args_brief "--logo-type $fetch_logo_type --logo-width 24 --logo \"$HOME/.config/fastfetch/logo_ros/ros.png\" --color \"#74c7ec\""
+    set -g fetch_args_brief "--logo-type $fetch_logo_type --logo-width 28 --logo \"$HOME/.config/fastfetch/logo_ros/ros.png\" --color \"#74c7ec\""
 end
 
 if type -q fastfetch
