5aec24f5422a08d694a6c6bb30d485fc7f23a8f4
Uyanide <pywang0608@foxmail.com>
2025-09-24 18:33:20 +0200

    delete extension names of scripts

--- eww/Lyrics/scripts/lyric-clear.sh
+++ eww/Lyrics/scripts/lyric-clear.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
-    "$HOME/.local/bin/spotify-lyrics" clear
+if [ -z "$1" ]; then
+    "spotify-lyrics" clear
     notify-send -a "spotify-lyrics" "Cache Cleared" "Lyrics cache have been cleared."
 else
-    "$HOME/.local/bin/spotify-lyrics" clear "$1"
+    "spotify-lyrics" clear "$1"
     notify-send -a "spotify-lyrics" "Cache Cleared" "Lyrics cache for track $1 have been cleared."
 fi
