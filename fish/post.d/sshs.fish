012ff152c6e8bcfd90558062f0e470d8d5fbbab7
Uyanide <pywang0608@foxmail.com>
2025-09-30 01:41:20 +0200

    better ssh

--- fish/post.d/sshs.fish
+++ fish/post.d/sshs.fish
@@ -1,22 +1,7 @@
 # ssh with encrypted private keys
 # $ssh_keys should be set in advance or left empty to use default keys
 if type -q ssh
-    # start ssh agent if not already running
-    if not set -q SSH_AUTH_SOCK
-        if test -f "$HOME/.ssh-agent" -a -r "$HOME/.ssh-agent"
-            bass source "$HOME/.ssh-agent" > /dev/null 2>&1
-            # check if the socket is actually working
-            if not ssh-add -l > /dev/null 2>&1
-                command rm -f "$HOME/.ssh-agent"
-                set -e SSH_AUTH_SOCK
-            end
-        end
-
-        if not set -q SSH_AUTH_SOCK 2>&1
-            command rm -f "$HOME/.ssh-agent"
-            bass eval (ssh-agent -s | tee "$HOME/.ssh-agent") > /dev/null 2>&1
-        end
-    end
+    bass $(ssh-init) > /dev/null 2>&1
 
     # only need to input passphrase once per session
     function sshs
