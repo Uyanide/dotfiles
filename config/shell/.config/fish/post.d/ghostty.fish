f23fe5506d18db487027af57052536a2c0885798
Uyanide <pywang0608@foxmail.com>
2025-12-06 18:14:48 +0100

    fish: same as last

--- config/shell/.config/fish/post.d/ghostty.fish
+++ config/shell/.config/fish/post.d/ghostty.fish
@@ -1,4 +1,4 @@
-if string match -q '*ghostty*' $TERM && test $XDG_CURRENT_DESKTOP = niri
+if string match -q '*ghostty*' $TERM; and test -n "$XDG_CURRENT_DESKTOP"; and test "$XDG_CURRENT_DESKTOP" = "niri"
     if test "$ghostty_niri_initialized" != "1"
         set -xg ghostty_niri_initialized 1
         niri msg action set-column-width 50%
