af081f5669d2f76d220c6bc6af5c4cf91f0ccd0a
Uyanide <pywang0608@foxmail.com>
2025-12-29 16:31:41 +0100

    fish: fix a stupid mistake

--- config/shell/.config/fish/prev.d/theme.fish
+++ config/shell/.config/fish/prev.d/theme.fish
@@ -1,6 +1,6 @@
-set -l target_ver 3.4.0
+set -l target_ver 4.3.0
 
-if test (string join \n $FISH_VERSION $target_ver | sort -V | head -n 1) = $target_ver
+if test (string join \n $FISH_VERSION $target_ver | sort -V | tail -n 1) = $target_ver
     set -l theme 'Catppuccin Mocha'
 
     if not set -q fish_current_theme; or not string match -q "$theme" "$fish_current_theme"
