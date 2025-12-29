1740c76c158b7e81e0f0fa907783311c2a440a6d
Uyanide <pywang0608@foxmail.com>
2025-12-29 16:29:21 +0100

    fish: update

--- config/shell/.config/fish/prev.d/theme.fish
+++ config/shell/.config/fish/prev.d/theme.fish
@@ -1,6 +1,10 @@
-set -l theme 'Catppuccin Mocha'
+set -l target_ver 3.4.0
 
-if not set -q fish_current_theme; or not string match -q "$theme" "$fish_current_theme"
-    set -U fish_current_theme "$theme"
-    fish_config theme save "$theme"
+if test (string join \n $FISH_VERSION $target_ver | sort -V | head -n 1) = $target_ver
+    set -l theme 'Catppuccin Mocha'
+
+    if not set -q fish_current_theme; or not string match -q "$theme" "$fish_current_theme"
+        set -U fish_current_theme "$theme"
+        fish_config theme save "$theme"
+    end
 end
