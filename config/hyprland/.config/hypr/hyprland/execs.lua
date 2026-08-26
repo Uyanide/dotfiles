e508025859faaca5ea76fe678100d573fd5e48f6
Uyanide <pywang0608@foxmail.com>
2026-08-26 16:19:37 +0200

    danshari!

--- config/hyprland/.config/hypr/hyprland/execs.lua
+++ config/hyprland/.config/hypr/hyprland/execs.lua
@@ -1,7 +1,7 @@
 hl.on("hyprland.start", function()
     hl.exec_cmd("uwsm finalize HYPRLAND_INSTANCE_SIGNATURE")
     hl.exec_cmd("waybar")
-    hl.exec_cmd("parra")
+    -- hl.exec_cmd("parra")
     hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
     hl.exec_cmd("/usr/lib/soteria-polkit/soteria")
     hl.exec_cmd("solaar -w hide")
--- legacy/eww/.config/eww/Lyrics/scripts/lyric-clear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1" ]; then
-    "spotify-lyrics" clear
-    notify-send -a "spotify-lyrics" "Cache Cleared" "Lyrics cache have been cleared."
-else
-    "spotify-lyrics" clear "$1"
-    notify-send -a "spotify-lyrics" "Cache Cleared" "Lyrics cache for track $1 have been cleared."
-fi
